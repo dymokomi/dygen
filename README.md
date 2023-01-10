9bd8c8eafb23ded72e52565d434bafafabe1bfff
Dy Mokomi <119466704+dymokomi@users.noreply.github.com>
2023-01-10 15:04:45 -0800

    docs

--- README.md
+++ README.md
@@ -1,4 +1,46 @@
 # dygen
-Generative tools for post processing
+Post processing tool.
 
-# put images in /ref folder and run 'python dygen.py' and you'll get images in the /out folder.
+## Pre-requisite
+
+You will need following Python libraries installed:
+
+PIL and tqdm
+These commands should help:
+```
+pip install 
+pip install Pillow
+```
+
+# Use
+
+Run
+```
+python dygen.py
+``` 
+and you'll get images in the /out folder.
+
+The script currently turns this image:
+![source_image](ref/ref.png)
+
+into this:
+![source_image](out/ref.sharpened.v1.png)
+
+# How to do this with your own images:
+
+1. Place an image in a ref folder. Make sure it's a .png!
+2. Open dygen.py and change ```init_file = "ref"```, where ```ref``` is the name of your file without extension. I.e. if your file is named ```artwork.png```, change the line to ```init_file = "artwork"```
+
+3. Run the script with ```python dygen.py``` 
+4. You'll get three new files in /out folder named 
+  - artwork.blobs.v1.png - raw color blobs
+  - artwork.painted.v1.png - finished painting
+  - artwork.sharpened.v1.png - a sharper looking version of the painting
+
+# How to be more creative.
+
+Open dygen.py and start messing up with parameters. They're at the top of the file and I tried to document them as much as possible. Play with different numbers, like ```cluster``` and ```color_count``` and see what you get.
+
+# Advanced 
+
+The fun start when you start using your own stencil masks. In /tex folder I placed two files that are currently used in the painting process, if you edit those, you'll get a much different look. Think of them as brushes. Worse comes to worse - hit me up on Twitter @dymokomi
--- dygen.py
+++ dygen.py
@@ -1,3 +1,5 @@
+
+
 from dg_lib import DYImage
 from tqdm import tqdm
 import random
@@ -16,8 +18,9 @@ glow_amount = (0.3, 0.3, 0.3) # amount of glow in each channel (R, G, B) should 
 
 
 
-# These are 
+# These are advanced settings
 root_location = os.path.dirname(__file__)
+number_of_masks = 2 # if you put more masks in the ref folder - bump up this number
 k = 1
 msk_m = pixel_step
 use_map = False 
@@ -44,14 +47,14 @@ masks = quantized_image.extract(palette)
 rounded_masks = []
 for mask in tqdm(masks): rounded_masks.append( mask.rounded(8, 110))
 stencil_masks = []
-stencil_masks.append( DYImage(f"{root_location}/tex/tex1.png").scaled( m * msk_m).expanded(source_image.size()))
-stencil_masks.append( DYImage(f"{root_location}/tex/tex2.png").scaled( m * msk_m ).expanded(source_image.size()))
+stencil_masks.append( DYImage(f"{root_location}/tex/tex1.png").scaled( scale_image * msk_m).expanded(source_image.size()))
+stencil_masks.append( DYImage(f"{root_location}/tex/tex2.png").scaled( scale_image * msk_m ).expanded(source_image.size()))
 for i, color in enumerate(tqdm(palette)): source_image.paint(masks[i], color)
 source_image.save(f"{root_location}/out/{init_file}.blobs.v{version}.png")
-for i, color in enumerate(tqdm(palette)): source_image.paint(rounded_masks[i].blur(12*2).clamp(10).multed(stencil_masks[random.randint(0,1)],threshold=False), color, amount=0.5, threshold=False, volume_diff=0.03)
+for i, color in enumerate(tqdm(palette)): source_image.paint(rounded_masks[i].blur(12*2).clamp(10).multed(stencil_masks[random.randint(0,number_of_masks-1)],threshold=False), color, amount=0.5, threshold=False, volume_diff=0.03)
 source_image2.blend(source_image, 0.5)
 source_image2 = source_image2.mult(1.05)
-for i, color in enumerate(tqdm(palette)): source_image.comp(source_image2, rounded_masks[i].multed(stencil_masks[random.randint(0,1)]))
+for i, color in enumerate(tqdm(palette)): source_image.comp(source_image2, rounded_masks[i].multed(stencil_masks[random.randint(0,number_of_masks-1)]))
 source_image = source_image.glow_blur(0.0, 15, glow_amount)
 source_image.save(f"{root_location}/out/{init_file}.painted.v{version}.png") 
 source_image = source_image.sharpen()
