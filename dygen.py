29e1c526d77c975fe4571fbdad7ad866ef277a76
Dy Mokomi <119466704+dymokomi@users.noreply.github.com>
2023-01-10 19:21:15 -0800

    a bit more options and docs

--- dygen.py
+++ dygen.py
@@ -1,5 +1,3 @@
-
-
 from dg_lib import DYImage
 from tqdm import tqdm
 import random
@@ -16,7 +14,9 @@ pixel_step      = 2     # how pixilated should the image be. Bigger number - lar
 glow_amount     = (0.3, 0.3, 0.3) # amount of glow in each channel (R, G, B) should be between 0.0 and 1.0
 stencils        = ["tex1", "tex2"] # list of textures you'd like to use (files in /tex folder)
 
-
+# Options for different features
+add_pieces_of_original_image = True
+add_glow = True
 
 # These are advanced settings
 root_location = os.path.dirname(__file__)
@@ -30,36 +30,59 @@ k *= scale_image
 
 
 
-
+# Prep work
 print(f"Starting {init_file}")
-random.seed(2)
-
+random.seed(2) # You can change this if you'd like brushes to be different
 source_image = DYImage(f"{root_location}/ref/{init_file}.png").scaled( scale_image )
 source_image2 = DYImage(f"{root_location}/ref/{init_file}.png").scaled( scale_image )
+
 print("Quantized")
 quantized_image = source_image.quantized(color_count, 1)
+
 print("Cleanup, Quantize")
 if cluster > 1 and use_map: quantized_image = quantized_image.cleanup_map(DYImage(f"{root_location}/ref/{init_file}.map.png"),3,cluster).rgb().quantized(color_count) 
 if cluster > 1 and not use_map: quantized_image = quantized_image.cleanup(cluster).quantized(color_count,1)
-print("Pixalated")
-if pixel_step > 1: quantized_image = quantized_image.pixilated((pixel_step*k,pixel_step*k), 0, 1.0, False)
-palette = quantized_image.palette()
-masks = quantized_image.extract(palette)
-rounded_masks = []
-for mask in tqdm(masks): rounded_masks.append( mask.rounded(8, 110))
-stencil_masks = []
 
-# Load all stencils
+print("Pixalated")
+if pixel_step > 1: 
+    quantized_image = quantized_image.pixilated((pixel_step*k,pixel_step*k), 0, 1.0, False)
+
+# Extract palette
+palette = quantized_image.palette()
+
+# Extract masks for each color based on palette
+rounded_masks = []
+masks = quantized_image.extract(palette)
+for mask in tqdm(masks): rounded_masks.append( mask.rounded(8, 110))
+
+
+# Load all stencils: These are essentialy brush textures
+stencil_masks = []
 for stencil in stencils:
     stencil_masks.append( DYImage(f"{root_location}/tex/{stencil}.png").scaled( scale_image * msk_m).expanded(source_image.size()))
 
+
+print("Begin painting")
+
+# This is where all the painting starts. At this point we have quantized image with blobs of color as a base
 for i, color in enumerate(tqdm(palette)): source_image.paint(masks[i], color)
 source_image.save(f"{root_location}/out/{init_file}.blobs.v{version}.png")
+
+# This is another stage that makes painting look a bit loose. 
+# It's where the masks are slighly extended and multiplied with brushes
 for i, color in enumerate(tqdm(palette)): source_image.paint(rounded_masks[i].blur(12*2).clamp(10).multed(stencil_masks[random.randint(0,number_of_masks-1)],threshold=False), color, amount=0.5, threshold=False, volume_diff=0.03)
 source_image2.blend(source_image, 0.5)
 source_image2 = source_image2.mult(1.05)
-for i, color in enumerate(tqdm(palette)): source_image.comp(source_image2, rounded_masks[i].multed(stencil_masks[random.randint(0,number_of_masks-1)]))
-source_image = source_image.glow_blur(0.0, 15, glow_amount)
+
+# Adds reminiscence of original image to define some of the shapes
+if add_pieces_of_original_image:
+    for i, color in enumerate(tqdm(palette)): source_image.comp(source_image2, rounded_masks[i].multed(stencil_masks[random.randint(0,number_of_masks-1)]))
+
+# Add glow to the image
+if add_glow:
+    source_image = source_image.glow_blur(0.0, 15, glow_amount)
 source_image.save(f"{root_location}/out/{init_file}.painted.v{version}.png") 
+
+# Make the image sharper
 source_image = source_image.sharpen()
 source_image.save(f"{root_location}/out/{init_file}.sharpened.v{version}.png") 
